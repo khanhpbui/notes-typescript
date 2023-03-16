b0e1ad6e5990bab7d502623a01cd99684e5b3184
Khanh Bui <pkkhanhbui@gmail.com>
2023-03-16 15:51:42 -0400

    functions to set up local storage

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,25 @@ import { Container } from "react-bootstrap";
 import { NewNote } from "./pages/NewNote";
 
 import { useMemo } from "react";
+import { useLocalStorage } from "./utils/useLocalStorage";
 
 export type Note = {
   id: string;
 } & NoteData;
 
-
 export type NoteData = {
   title: string;
   markdown: string;
   tags: Tag[];
+};
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagId: string[];
 };
 export type Tag = {
   id: string;
@@ -21,7 +30,9 @@ export type Tag = {
 };
 
 function App() {
- 
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("notes", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("tags", []);
+
   return (
     <Container className="my-4">
       <Routes>
